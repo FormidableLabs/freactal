d96077a791a203225e0cb6418555262d0408389d
Dale Bustad <dale@divmain.com>
2017-03-27 00:31:09 -0700

    Improve example.

--- example/src/effects.js
+++ example/src/effects.js
@@ -10,7 +10,10 @@ export const setDataPending = (effects, value) => hardUpdate({ pending: !!value 
 
 const delay = ms => val => new Promise(resolve => setTimeout(() => resolve(val), ms));
 
-export const fetchTodos = (effects, url) =>
-  wrapWithPending(effects, () => fetch(url).then(delay(2000)))
+export const fetchTodos = (effects, url) => wrapWithPending(
+  effects,
+  () => fetch(url).then(delay(2000))
     .then(result => result.json())
-    .then(json => update({ todos: json }));
+    .then(json => update({ todos: json }))
+);
+
