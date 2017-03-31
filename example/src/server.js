fa1eab60b64131fba5e798fa5be1bff4cca22105
Dale Bustad <dale@divmain.com>
2017-03-30 23:59:19 -0700

    Clean up SSR server example.

--- example/src/server.js
+++ example/src/server.js
@@ -1,9 +1,8 @@
-import path from "path";
-
 import React from "react";
 import { renderToString } from "react-dom/server";
 import express from "express";
-import serveStatic from "serve-static";
+import webpack from "webpack";
+import webpackMiddleware from "webpack-dev-middleware";
 import { initialize } from "../../server";
 
 import StatefulRootComponent from "./app";
@@ -15,8 +14,29 @@ Error.stackTraceLimit = Infinity;
 
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, "../build/static")));
-
+app.use(webpackMiddleware(webpack({
+  entry: "./src/index.js",
+  output: {
+    path: "/",
+    filename: "main.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      loader: "babel",
+      include: __dirname
+    }]
+  }
+}), {
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: true
+  },
+  publicPath: "/assets/",
+  stats: {
+    colors: true
+  }
+}));
 
 const boilerplate = (componentHtml, state) => `
   <html>
@@ -26,25 +46,20 @@ const boilerplate = (componentHtml, state) => `
       <script type="text/javascript">
         window.__state__ = ${JSON.stringify(state)};
       </script>
-      <script type="application/javascript" src="/js/main.js" defer></script>
+      <script type="application/javascript" src="/assets/main.js" defer></script>
     </body>
   </html>
 `;
 
 
 app.route("/").get((req, res) => {
-  initialize(<StatefulRootComponent />).then(({ vdom, state }) => {
+  initialize(<StatefulRootComponent rootProp="hello" />).then(({ vdom, state }) => {
     const appHtml = renderToString(vdom);
     const html = boilerplate(appHtml, state);
-    return res.send(html).end()
+    return res.send(html).end();
   });
 });
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}...`);
 });
-
-
-
-
-
