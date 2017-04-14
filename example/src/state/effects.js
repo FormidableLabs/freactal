075e8b127f752733c38573877b3ebcf3a57571fe
Dale Bustad <dale@divmain.com>
2017-04-14 00:52:16 -0700

    Cleaner `wrapWithPending` implementation in example app.

--- example/src/state/effects.js
+++ example/src/state/effects.js
@@ -3,19 +3,19 @@ import { softUpdate } from "../../..";
 
 const IS_BROWSER = typeof window === "object";
 
+const update = newState => state => Object.assign({}, state, newState);
 
-const wrapWithPending = (effects, cb) =>
-  effects.setDataPending(true)
-    .then(cb)
-    .then(value => effects.setDataPending(false).then(() => value));
+const wrapWithPending = cb => (effects, ...args) => effects.setDataPending(true)
+  .then(() => cb(...args))
+  .then(value => effects.setDataPending(false).then(() => value));
 
-export const setDataPending = softUpdate((state, value) => ({ pending: !!value }));
+export const setDataPending = softUpdate((state, value) => ({ pending: value }));
 
 const delay = ms => val => new Promise(resolve => setTimeout(() => resolve(val), ms));
 
-export const fetchTodos = (effects, url) => wrapWithPending(
-  effects,
-  () => fetch(url).then(delay(2000))
+export const fetchTodos = wrapWithPending(url =>
+  fetch(url)
+    .then(delay(2000))
     .then(result => result.json())
     .then(json => update({ todos: json }))
 );
