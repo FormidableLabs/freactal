38674ab658f973a95bd305e2379be40de5b5a836
Dale Bustad <dale@divmain.com>
2017-04-22 13:30:11 -0700

    Augment test setup with enzyme, chai-enzyme and jsdom.

--- spec/index.js
+++ spec/index.js
@@ -4,20 +4,42 @@ Error.stackTraceLimit = Infinity;
 
 const fs = require("fs");
 const path = require("path");
+const { jsdom } = require("jsdom");
 
 require("babel-core/register");
 
 const chai = require("chai");
 const sinonChai = require("sinon-chai");
+const chaiEnzyme = require("chai-enzyme");
 global.sinon = require("sinon");
 
 chai.config.includeStack = true;
 chai.use(sinonChai);
+chai.use(chaiEnzyme());
 
 global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
 global.assert = chai.assert;
+
+beforeEach(() => {
+  global.sandbox = sinon.sandbox.create();
+});
+
+afterEach(() => {
+  global.sandbox.restore();
+  delete global.sandbox;
+});
+
+const document = global.document = jsdom("");
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === "undefined") {
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = { userAgent: "node.js" };
 
 const specFile = /\.spec\.js$/;
 const recursiveRequire = (basepath, cb) => fs.readdirSync(basepath).forEach(filename => {
