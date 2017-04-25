cfbadde2f65645d17c8800d548aa63517c6e9845
Dale Bustad <dale@divmain.com>
2017-04-25 00:27:10 -0700

    Extract inject methods out into base class.

--- src/inject.js
+++ src/inject.js
@@ -3,9 +3,61 @@ import { default as React, Component } from "react";
 import { contextTypes } from "./context";
 
 
-export const injectState = (StatelessComponent, keys = null) => {
-  const auto = !keys;
+export class BaseInjectStateHoc extends Component {
+  componentDidMount () {
+    this.mounted = true;
+    this.unsubscribe = this.context.freactal.subscribe(this.update.bind(this));
+  }
 
+  componentWillReceiveProps () {
+    this.usedKeys = null;
+  }
+
+  componentWillUnmount () {
+    this.mounted = false;
+    this.unsubscribe();
+  }
+
+  update (changedKeys) {
+    if (this.mounted && this.shouldUpdate(changedKeys, this.usedKeys)) {
+      this.forceUpdate();
+    }
+  }
+
+  getTrackedState () {
+    const state = this.context.freactal.state;
+    const trackedState = Object.create(null);
+    const usedKeys = this.usedKeys = Object.create(null);
+
+    Object.keys(state).forEach(key => {
+      usedKeys[key] = false;
+
+      Object.defineProperty(trackedState, key, {
+        enumerable: true,
+        get () {
+          usedKeys[key] = true;
+          return state[key];
+        }
+      });
+    });
+
+    return trackedState;
+  }
+
+  render () {
+    const state = this.auto ? this.getTrackedState() : this.context.freactal.state;
+
+    return (
+      <this.StatelessComponent
+        {...this.props}
+        state={state}
+        effects={this.context.freactal.effects}
+      />
+    );
+  }
+}
+
+export const injectState = (StatelessComponent, keys = null) => {
   const shouldUpdate = keys ?
     changedKeys => keys.reduce((memo, key) => memo || changedKeys[key], false) :
     (changedKeys, usedKeys) => usedKeys ?
@@ -14,64 +66,16 @@ export const injectState = (StatelessComponent, keys = null) => {
         .reduce((memo, key) => memo || changedKeys[key], false) :
       true;
 
-  class InjectStateHoc extends Component {
+  class InjectStateHoc extends BaseInjectStateHoc {
     constructor (...args) {
       super(...args);
       if (!this.context.freactal) {
         throw new Error("Attempted to inject state without parent Freactal state container.");
       }
-    }
 
-    componentDidMount () {
-      this.mounted = true;
-      this.unsubscribe = this.context.freactal.subscribe(this.update.bind(this));
-    }
-
-    componentWillReceiveProps () {
-      this.usedKeys = null;
-    }
-
-    componentWillUnmount () {
-      this.mounted = false;
-      this.unsubscribe();
-    }
-
-    update (changedKeys) {
-      if (this.mounted && shouldUpdate(changedKeys, this.usedKeys)) {
-        this.forceUpdate();
-      }
-    }
-
-    getTrackedState () {
-      const state = this.context.freactal.state;
-      const trackedState = Object.create(null);
-      const usedKeys = this.usedKeys = Object.create(null);
-
-      Object.keys(state).forEach(key => {
-        usedKeys[key] = false;
-
-        Object.defineProperty(trackedState, key, {
-          enumerable: true,
-          get () {
-            usedKeys[key] = true;
-            return state[key];
-          }
-        });
-      });
-
-      return trackedState;
-    }
-
-    render () {
-      const state = auto ? this.getTrackedState() : this.context.freactal.state;
-
-      return (
-        <StatelessComponent
-          {...this.props}
-          state={state}
-          effects={this.context.freactal.effects}
-        />
-      );
+      this.auto = !keys;
+      this.shouldUpdate = shouldUpdate;
+      this.StatelessComponent = StatelessComponent;
     }
   }
 
