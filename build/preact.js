761dab089a5b3d7fd278f88f57a388cab08a853d
Dale Bustad <dale@divmain.com>
2017-05-02 16:32:19 -0700

    Add initial client-side Preact support via Babel transform over codebase.

---
+++ build/preact.js
@@ -0,0 +1,44 @@
+const t = require("babel-types");
+
+module.exports = () => ({
+  visitor: {
+    ImportDeclaration: path => {
+      if (path.node.source.value === "react") {
+        path.node.source.value = "preact";
+        path.node.specifiers = path.node.specifiers
+          .filter(specifier => {
+            if (specifier.local.name === "React") { return false; }
+            return true;
+          })
+          .concat([
+            t.importSpecifier(
+              t.identifier("h"),
+              t.identifier("h")
+            )
+          ]);
+      } else if (
+        /\/context$/.test(path.node.source.value) &&
+        path.node.specifiers.reduce((memo, specifier) => {
+          return memo || specifier.imported.name === "contextTypes";
+        }, false)
+        // import { contextTypes } from "./context";
+      ) {
+        path.remove();
+      } else if (path.node.source.value === "react-dom") {
+        path.node.source.value = "preact";
+      }
+    },
+    AssignmentExpression: path => {
+      if (
+        path.node.right.type === "Identifier" &&
+        path.node.right.name === "contextTypes" &&
+        path.node.left.type === "MemberExpression" &&
+        path.node.left.property.type === "Identifier" &&
+        (path.node.left.property.name === "contextTypes" ||
+         path.node.left.property.name === "childContextTypes")
+      ) {
+        path.parentPath.remove();
+      }
+    }
+  }
+});
