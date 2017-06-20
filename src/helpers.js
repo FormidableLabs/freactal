28673448abbfb2b77cea6a6634d04d88b673ea07
Dale Bustad <dale@divmain.com>
2017-06-20 00:05:13 -0700

    Throw helpful error if invalid object is provided to `update` helper.
    Relates to #14.

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,6 @@
 let warningShown = false;
 const displayDeprecationMessage = () => {
-  if (warningShown) {
-    return;
-  }
+  if (warningShown) { return; }
   warningShown = true;
   // eslint-disable-next-line no-console
   console.log("Both `hardUpdate` and `softUpdate` are deprecated.  Please use `update` instead.");
@@ -12,10 +10,22 @@ export const hardUpdate = newState => {
   displayDeprecationMessage();
   return () => state => Object.assign({}, state, newState);
 };
+
 export const softUpdate = fn => {
   displayDeprecationMessage();
   return (effects, ...args) => state => Object.assign({}, state, fn(state, ...args));
 };
-export const update = fnOrNewState =>
-  typeof fnOrNewState === "function" ? softUpdate(fnOrNewState) : hardUpdate(fnOrNewState);
+
+export const update = fnOrNewState => {
+  if (typeof fnOrNewState === 'function') {
+    return softUpdate(fnOrNewState)
+  }
+
+  if (typeof fnOrNewState === 'object') {
+    return hardUpdate(fnOrNewState)
+  }
+
+  throw new Error("update must receive a reducer function or object to merge as its argument.");
+}
+
 export const mergeIntoState = dataToMerge => state => Object.assign({}, state, dataToMerge);
